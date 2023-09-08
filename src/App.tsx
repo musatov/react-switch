0fd76bdabd77aa8c8313ff4c2f3b7c48b3d2ead5
Michael Musatov <mv.musatov@gmail.com>
2023-09-08 12:15:16 +0400

    Basic implementation of Switch component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
+import { useState } from 'react'
 import './App.css'
 import { Switch } from './Switch'
 
 function App() {
+  const [condition, setCondition] = useState<undefined | string>()
 
   return (
     <>
       <h3>React Switch Component</h3>
 
-      <Switch condition={4}>
+      <div>
+        <label htmlFor="select-input">Possible values:</label>
+        <select id="select-input" onChange={(e) => setCondition(e.target?.value)}>
+          <option>Not defined</option>
+          <option value="1">1</option>
+          <option value="2">2</option>
+        </select>
+      </div>
+
+      <Switch condition={condition}>
         <Switch.Case when={1}>
           <div>1</div>
         </Switch.Case>
--- src/Switch.tsx
+++ src/Switch.tsx
@@ -7,10 +7,11 @@ interface CaseProps {
 
 interface DefaultProps {
   children?: ReactNode
+  when?: never
 }
 
 interface SwitchComponentProps {
-  condition: string | number
+  condition: undefined | string | number
   children?: 
     | ReactElement<CaseProps | DefaultProps>| ReactElement<CaseProps | DefaultProps>[]
 }
@@ -21,12 +22,22 @@ interface SwitchComponentType extends FC <SwitchComponentProps> {
 }
 
 export const Switch: SwitchComponentType = ({ condition, children }) => {
-  console.log('condition', condition)
-  return <>{children}</>
+  if (!children) {
+    return null
+  }
+
+  const arrayOfChildren = Array.isArray(children) ? children : [children]
+  const cases = arrayOfChildren.filter((child) => child.props.when == condition)
+  const defaultCases = arrayOfChildren.filter((child) => !child.props.when)
+  if (defaultCases.length > 1) {
+    throw new Error('Only one <Switch.Default> is allowed')
+  }
+  const defaultCase = defaultCases[0]
+
+  return cases.length > 0 ? <>{cases}</> : <>{defaultCase}</>
 }
 
-Switch.Case = ({ children, when }) => {
-  console.log('when', when)
+Switch.Case = ({ children }) => {
   return <>{children}</>
 }
 
